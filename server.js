932d2dac2844a59df3fd169b99f4f48d7f67801b
DHABBU <shahaneri107@gmail.com>
2016-09-21 11:59:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article_one',function (req,res){
-     res.sendFile(path.join(_dirname,'ui','article_one.html'));
+       res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
