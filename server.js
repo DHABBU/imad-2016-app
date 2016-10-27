3ff55db2a9ff19d0b85f38ae1034b7fc68818247
DHABBU <shahaneri107@gmail.com>
2016-10-27 16:08:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:'ARTICLE ONE| ANERI SHAH',
+    heading:'ARTICLE ONE',
+    date:'SEP 21',
+    content:`<p>
+                This is my first html content file for making an web app.
+                
+            </p>`
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+    var htmlTemplate=`<html>
+         <head>
+             <title> ${title} </title>
+             <meta name="vi export" content="width=device-width, initial scale=1"/>
+              <link href="/ui/style.css" rel="stylesheet" />
+         
+         </head>
+        <body>
+            <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr/>
+            <div>
+                <h3>${heading}</h3>
+            </div>
+            <div>
+                ${date}
+            </div>
+            <div>
+                ${content}
+            </div>
+            </div>
+            
+        </body>
+        
+        
+    </html>`
+
+}
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
